312b7e816b811caf58556229dec04271da6f73be
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-22 23:05:00 +0300

    Add test

--- demoqa_tests/data/users.py
+++ demoqa_tests/data/users.py
@@ -1,11 +1,62 @@
 import dataclasses
+from datetime import date
+from typing import List
 
+
+class Gender:
+    male = 'Male'
+    female = 'Female'
+    other = 'Other'
+
+
+class Subject:
+    accounting = 'Accounting'
+    arts = 'Arts'
+    biology = 'Biology'
+    english = 'English'
+    chemistry = 'Chemistry'
+    computer_science = 'Computer Scien'
+    commerce = 'Commerce'
+    maths = 'Maths'
+    physics = 'Physics'
+    economics = 'Economics'
+    social_studies = 'Social Studies'
+
+
+class Hobby:
+    sports = 'Sports'
+    reading = 'Reading'
+    music = 'Music'
 
 @dataclasses.dataclass
 class User:
-    full_name: str
+    first_name: str
+    last_name: str
     email: str
+    gender: Gender
+    number: str
+    birthday: date
+    subjects: List[Subject]
+    hobbies: List[Hobby]
+    picture: str
+    address: str
+    state: str
+    city: str
 
 
-admin = User(full_name='admina adminovych', email='super+admin@gmail.com')
-guest = User('harry', 'potter@hg.com')
+user = User(
+    first_name='Ivan',
+    last_name='YA',
+    email='name@example.com',
+    gender=Gender.male,
+    number='1234567891',
+    birthday=date(1999, 5, 11),
+    subjects=Subject.maths,
+    hobbies=Hobby.reading,
+    picture='test.png',
+    address='Bronnya Street 14',
+    state='NCR',
+    city='Delhi')
+
+# admin = User(full_name='admina adminovych', email='super+admin@gmail.com')
+# guest = User('harry', 'potter@hg.com')
--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -1,60 +1,82 @@
+import os
+
 from selene import have, command
 from selene.support.shared import browser
 
+from demoqa_tests import resource
+from demoqa_tests.data.users import User
+
 
 class RegistrationPage:
-    def __init__(self):
-        self.first_name = browser.element('#firstName')
-        self.last_name = browser.element('#lastName')
-        self.state = browser.element('#state')
+    # def __init__(self):
+    #     self.first_name = browser.element('#firstName')
+    #     self.last_name = browser.element('#lastName')
+    #     self.state = browser.element('#state')
 
+    # def open(self):
+    #     browser.open('/automation-practice-form')
+    #     browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
+    #         have.size_greater_than_or_equal(3)
+    #     )
+    #     browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+    #     return self
+    #
+    # def fill_first_name(self, value):
+    #     self.first_name.type(value)
+    #     return self
+    #
+    # def fill_last_name(self, value):
+    #     self.last_name.type(value)
+    #     return self
+    #
+    # def fill_date_of_birth(self, year, month, day):
+    #     browser.element('#dateOfBirthInput').click()
+    #     browser.element('.react-datepicker__month-select').type(month)
+    #     browser.element('.react-datepicker__year-select').type(year)
+    #     browser.element(
+    #         f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+    #     ).click()
+    #     return self
+    #
+    # def fill_state(self, name):
+    #     self.state.perform(command.js.scroll_into_view)
+    #     self.state.click()
+    #     browser.all('[id^=react-select][id*=option]').element_by(
+    #         have.exact_text(name)
+    #     ).click()
+    #     return self
+    #
+    # def should_registered_user_with(self, full_name, email, gender):
+    #     # todo: refactor to reuse parameters
+    #     browser.element('.table').all('td').even.should(
+    #         have.exact_texts(
+    #             full_name,
+    #             email,
+    #             gender,
+    #             '1234567891',
+    #             '11 May,1999',
+    #             'Computer Science',
+    #             'Reading',
+    #             'foto.jpg',
+    #             'Moscowskaya Street 18',
+    #             'NCR Delhi',
+    #         )
+    #     )
+    #     return self
     def open(self):
         browser.open('/automation-practice-form')
-        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
-            have.size_greater_than_or_equal(3)
-        )
-        browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
-        return self
 
-    def fill_first_name(self, value):
-        self.first_name.type(value)
-        return self
+    def registration(self, user: User):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('[id="userEmail"]').send_keys(user.email)
+        browser.element('[name="gender"][value=Male]').double_click()
+        browser.element('[id="userNumber"]').send_keys(user.number)
+        browser.element('[id="dateOfBirthInput"]').click()
+        browser.element('.react-datepicker__year-select').type(user.birthday)
+        browser.element('.react-datepicker__month-select').type(user.birthday.strftime('%B'))
+        browser.element('.react-datepicker__day--011').click()
+        # for subject in user.subjects:
+        #     browser.element('#subjectsInput').type(user.subjects).press_enter()
 
-    def fill_last_name(self, value):
-        self.last_name.type(value)
-        return self
-
-    def fill_date_of_birth(self, year, month, day):
-        browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
-        browser.element(
-            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
-        ).click()
-        return self
-
-    def fill_state(self, name):
-        self.state.perform(command.js.scroll_into_view)
-        self.state.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(name)
-        ).click()
-        return self
-
-    def should_registered_user_with(self, full_name, email, *tbd):
-        # todo: refactor to reuse parameters
-        browser.element('.table').all('td').even.should(
-            have.exact_texts(
-                full_name,
-                email,
-                'Female',
-                '1234567891',
-                '11 May,1999',
-                'Computer Science',
-                'Reading',
-                'foto.jpg',
-                'Moscowskaya Street 18',
-                'NCR Delhi',
-            )
-        )
-        return self
+        browser.element('#uploadPicture').send_keys(os.getcwd() + f"/{user.picture}")
