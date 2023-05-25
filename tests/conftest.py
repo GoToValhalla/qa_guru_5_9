a1daa0379a4da1ba98bdbcc8b6278528ed813fca
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-05-25 18:58:47 +0300

    refactoring

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
+    browser.driver.set_window_size(1920, 1080)
     browser.config.base_url = 'https://demoqa.com'
 
     yield
